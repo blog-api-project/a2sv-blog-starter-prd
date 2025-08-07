19d213e3f7f762d74901845037d17dc418243b92
Zufan-Hadgu <zufanhadgu1@gmail.com>
2025-08-07 11:48:02 +0300

    maing.go and router edited

--- blog_api/Delivery/routers/router.go
+++ blog_api/Delivery/routers/router.go
@@ -1,28 +1,39 @@
 package routers
 
 import (
-	"blog_api/Delivery/controllers"
+  "blog_api/Delivery/controllers"
+  "blog_api/Domain/contracts/services"
+  infrastructure "blog_api/Infrastructure"
 
-	"github.com/gin-gonic/gin"
+  "github.com/gin-gonic/gin"
 )
 
-func SetupRouter(userController *controllers.UserController, tokenController *controllers.TokenController) *gin.Engine {
-	router := gin.Default()
+func SetupRouter(userController *controllers.UserController, tokenController *controllers.TokenController,blogController *controllers.BlogController,jwtService services.IJWTService,
+) *gin.Engine {
+  router := gin.Default()
 
-	// User routes
-	userRoutes := router.Group("/api/users")
-	{
-		userRoutes.POST("/register", userController.Register)
-		userRoutes.POST("/login", userController.Login)
-		userRoutes.POST("/logout", userController.Logout)
-	}
+  // User routes
+  userRoutes := router.Group("/api/users")
+  {
+    userRoutes.POST("/register", userController.Register)
+    userRoutes.POST("/login", userController.Login)
+    userRoutes.POST("/logout", userController.Logout)
+  }
 
-	// Authentication routes
-	authRoutes := router.Group("/api/auth")
-	{
-		authRoutes.POST("/refresh", tokenController.RefreshToken)
-		authRoutes.POST("/validate", tokenController.ValidateToken)
-	}
+  // Authentication routes
+  authRoutes := router.Group("/api/auth")
+  {
+    authRoutes.POST("/refresh", tokenController.RefreshToken)
+    authRoutes.POST("/validate", tokenController.ValidateToken)
+  }
 
-	return router
-} 
+  //Blog Router
+  blogRoutes := router.Group("/api/blogs")
+  blogRoutes.Use(infrastructure.AuthMiddleware(jwtService))
+
+  {
+         blogRoutes.POST("/create", blogController.CreateBlog)
+  }
+
+  return router
+}
